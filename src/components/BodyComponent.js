83f8db6483f626d1a54c092c898e03f0a01315e0
Jean-Eric Pierre-Louis <55325769+Nduriel@users.noreply.github.com>
2020-04-19 20:05:34 -0400

    4/19

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+// import Carousel from 'react-bootstrap/Carousel';
 
-const step1 = require('../images/step1mia.jpg');
-const step2 = require('../images/step2mia.jpg');
-const step3 = require('../images/step3mia.jpg');
-const step4 = require('../images/step4mia.jpg');
-const step5 = require('../images/step5mia.jpg');
+
+// const step1 = require('../images/step1mia.jpg');
+// const step2 = require('../images/step2mia.jpg');
+// const step3 = require('../images/step3mia.jpg');
+// const step4 = require('../images/step4mia.jpg');
+// const step5 = require('../images/step5mia.jpg');
 const bodypic1 = require('../images/BodCollage1.jpg');
-const bookappt = require('../images/bookappt.jpg');
-const prices = require('../images/pricesmia.jpg');
+const bookappt = require('../images/bookappt2.jpg');
+const prices = require('../images/prices2.jpg');
 
 class BodyComponent extends Component {
     render() {
         return (
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-lg-6 col-md-12 col-sm-12" style={{ height: "100%" }}>
-                        <img src={bodypic1} fluid id="heightpic" alt="bodypic1" />
+                    <div className="col-lg-6 col-sm-12">
+                        <img src={bookappt} fluid id="heightpic" alt="booking" style={{borderradius: "50%"}}/>
                     </div>
-                    <div className="col-lg-5 offset-lg-1 col-md-12 col-sm-12">
-                        <img src={bookappt} fluid style={{ width: "100%", height: "91%", paddingTop: "10px" }} alt="booking" />
+                    <div className="col-lg-6 col-sm-12">
+                        <img id="heightpic1" src={prices} alt="pricepic" />
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-lg-5 offset-lg-1" style={{ textAlign: "center" }}>
-                        <img id="heightpic1" src={prices} alt="pricepic"/>
-                    </div>
+                {/* <div className="row">
+                    
                     <div className="col-lg-4 offset-lg-1 col-md-6 col-sm-12">
                         <Carousel id="carouselborder">
                             <Carousel.Item>
@@ -50,7 +49,7 @@ class BodyComponent extends Component {
                             </Carousel.Item>
                         </Carousel>
                     </div>
-                </div>
+                </div> */}
             </div>
         );
     }
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react';
 import {
     Jumbotron, Navbar, Nav, NavItem,
-    Form, FormGroup, Label, Input,
+    Label, Button, Modal, ModalHeader,
+    ModalBody, ModalFooter, Col, Row,
 } from 'reactstrap';
 import Image from 'react-bootstrap/Image';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { DatePicker } from 'react-datepicker';
 
 const logo = require('../images/pbmimg13.png');
 const miaPic1 = require('../images/miaPic3.jpg');
 const miaPic2 = require('../images/miaPic2.jpg');
 
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+const isNumber = val => !isNaN(+val);
+const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
+
+const spancolor = {
+    color: "white"
+}
 class Header extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isModalOpen: false
+            isModalOpen: false,
+            firstName: '',
+            lastName: '',
+            phoneNum: '',
+            email: '',
+            time: '',
+            date: '',
+            touched: {
+                firstName: false,
+                lastName: false,
+                phoneNum: false,
+                email: false,
+                date: false
+            }
         };
-
         this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    handleSubmit(values) {
+        console.log('Current state is: ' + JSON.stringify(values));
+        alert('Current state is: ' + JSON.stringify(values));
     }
 
     toggleModal() {
@@ -33,7 +63,7 @@ class Header extends Component {
                     <div className='container-fluid'>
                         <div className='row' style={{ textAlign: 'center', fontSize: "22px" }}>
                             <div className='col-4'>
-                                <Image  id="jpic" style={{ height: "200px" }} src={miaPic2} alt="jpic1" roundedCircle />
+                                <Image id="jpic" style={{ height: "200px" }} src={miaPic2} alt="jpic1" roundedCircle />
                             </div>
                             <div className='col-4'>
                                 <Image style={{ height: "200px" }} src={logo} alt="logopic" />
@@ -44,13 +74,13 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
-                <Navbar color="#9df5cc" light expand="sm" sticky="top">
+                <Navbar light expand="sm" sticky="top">
                     <div className="container-fluid">
                         <div className="row row-cols-1">
                             <Nav navbar>
-                                <span style={{color:"#f1069f"}}>Hampton Roads Only!</span>
+                                <span style={{ color: "pink" }}>Hampton Roads Only!</span>
                                 <NavItem className="col-4">
-                                    <Button color="primary" onClick={this.toggleModal}                                   
+                                    <Button color="primary" onClick={this.toggleModal}
                                         style={{ color: "white", fontWeight: "bold", fontSize: "22px" }}>Appointments</Button>
                                 </NavItem>
                             </Nav>
@@ -79,36 +109,130 @@ class Header extends Component {
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal} className={this.props.className}>
                     <ModalHeader style={{ backgroundColor: "#680146", color: "#9df5cc", borderBottomColor: "purple" }} toggle={this.toggleModal}>Send me your info and I'll contact you asap!</ModalHeader>
                     <ModalBody id="modalbody">
-                        <Form>
-                            <FormGroup>
-                                <Label for="firstname">Name</Label>
-                                <Input type="text" name="firstname" id="firstname" placeholder="First Name" /> <br />
-                                <Input type="text" name="lastname" id="lastname" placeholder="Last Name" />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="email">Contact Information</Label>
-                                <Input type="email" name="email" id="emailaddress" placeholder="Email Address" />  <br />
-                                <Input type="tel" name="phonenumber" id="phonenumber" placeholder="Phone Number" />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="booking">Preferred day and time</Label><br />
-                                <div className="row">
-                                    <div className="col-6">
-                                        <Input type="date" name="date" id="date" /><br />
+                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                            <Row className="formgroup">
+                                <Col md={10}>
+                                    <Label htmlFor="firstName" style={{ fontWeight: "bold" }}>First Name</Label>
+                                    <Control.text model=".firstName" name="firstName" id="firstName"
+                                        placeholder="First Name"
+                                        className="form-control"
+                                        validators={{
+                                            required,
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".firstName"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: <span style={spancolor}>'Required'</span>,
+                                            minLength: <span style={spancolor}>'Must be at least 2 characters'</span>,
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                    <Label htmlFor="lastName" style={{ fontWeight: "bold" }}>Last Name</Label>
+                                    <Control.text model=".lastName" name="lastName" id="lastName"
+                                        placeholder="Last Name"
+                                        className="form-control"
+                                        validators={{
+                                            required,
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".lastName"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: <span style={spancolor}>'Required'</span>,
+                                            minLength: <span style={spancolor}>'Must be at least 2 characters'</span>,
+                                            maxLength: <span style={spancolor}>'Must be 15 characters or less'</span>
+                                        }}
+                                    />
+                                </Col>
+                            </Row><br />
+                            <Row className="formgroup">
+                                <Col md={10}>
+                                    <Label htmlFor="email" style={{ fontWeight: "bold" }}>Email Address</Label>
+                                    <Control.text model=".email" name="email" id="emailaddress"
+                                        placeholder="Email"
+                                        className="form-control"
+                                        validators={{
+                                            required,
+                                            validEmail
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".email"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: <span style={spancolor}>'Required'</span>,
+                                            validEmail: <span style={spancolor}>'Invalid email address'</span>
+                                        }}
+                                    />
+                                    <Label htmlFor="phoneNum" style={{ fontWeight: "bold" }}>Phone</Label>
+                                    <Control.text model=".phoneNum" name="phoneNum" id="phoneNum"
+                                        placeholder="Phone number"
+                                        className="form-control"
+                                        validators={{
+                                            required,
+                                            minLength: minLength(10),
+                                            maxLength: maxLength(15),
+                                            isNumber
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".phoneNum"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: <span style={spancolor}>'Required'</span>,
+                                            minLength: <span style={spancolor}>'Must be at least 10 numbers'</span>,
+                                            maxLength: <span style={spancolor}>'Must be 15 numbers or less'</span>,
+                                            isNumber: <span style={spancolor}>'Must be a number'</span>
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <br />
+                            <Row className="formgroup">
+                                <Col md={11}>
+                                    <div className="row">
+                                        <div className="col-6">
+                                            <Label htmlFor="date" style={{ fontWeight: "bold" }}>Date Requested</Label>
+                                            <Control.select model=".date" type="date" name="date" id="date"
+                                                className="form-control"
+                                            />
+                                        </div>
+                                        <div className="col">
+                                            <Label htmlFor="time" style={{ fontWeight: "bold" }}>Time Requested</Label>
+                                            <Control.select model=".time" name="time" id="time"
+                                                className="form-control"
+                                            />
+                                        </div>
                                     </div>
-                                    <div className="col">
-                                        <Input type="time" name="time" id="time" />
-                                    </div>
-                                </div>
-                                <div style={{ textAlign: "center" }}>
-                                    **Date and time submitted are not guaranteed**<br />
-                                    **Allow 2 hours per appointment**</div>
-                            </FormGroup>
-                        </Form>
+                                </Col>
+                            </Row>
+                            <br />
+                            <Col md={10}>
+                                <Row className="formgroup">
+                                    <Button type="submit" color="info">Reserve</Button>{' '}
+                                    <Button color="danger" onClick={this.toggleModal}>Cancel</Button>
+                                </Row>
+                            </Col>
+                        </LocalForm>
                     </ModalBody>
-                    <ModalFooter style={{ backgroundColor: "#680146", borderTopColor: "purple" }}>
-                        <Button color="info" onClick={this.toggleModal}>Submit</Button>{' '}
-                        <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+                    <ModalFooter style={{ backgroundColor: "#680146", borderTopColor: "purple", color: "#9df5cc" }}>
+                        <Col>**Dates and time submitted are not guaranteed**<br />
+                                    **Allow 2 hours per appointment**</Col>
                     </ModalFooter>
                 </Modal>
             </React.Fragment>

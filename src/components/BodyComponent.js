d2b09c80f57db9043b4ce57475c929f1ff983dab
Jean-Eric Pierre-Louis <55325769+Nduriel@users.noreply.github.com>
2020-05-19 15:18:11 -0400

    Responsiveness pt 2.2

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import { Col, Row, Container } from 'reactstrap';
-import Table from 'react-bootstrap/Table';
+import TableComponent from './TableComponent';
 import Image from 'react-bootstrap/Image';
 import Carousel from 'react-bootstrap/Carousel';
 
-
-const tableBody = {
-    color: "#9df5cc",
-    fontWeight: "bold",
-    textShadow: "2px 2px 2px #f1069f",
-}
-
-const textCenter = {
-    textAlign: "center",
-}
 
 const capstyle = {
     fontWeight: "bold",
@@ -23,123 +13,59 @@ const capstyle = {
     fontFamily: "Courgette"
 }
 const galpics = {
-    longNails: require('../images/miaPic26.jpg'),
-    medNails: require('../images/miaPic18.jpg'),
-    shortNails: require('../images/miaPic10.jpg'),
-    tpic: require('../images/noticepic.jpg'),
     serveArea: require('../images/BodCollage1.jpg'),
     pic4: require('../images/miaPic4.jpg'),
     pic5: require('../images/miaPic5.jpg'),
     pic6: require('../images/miaPic6.jpg'),
     pic7: require('../images/miaPic7.jpg'),
-    pic12: require('../images/miaPic11.jpg'),
+    pic12: require('../images/miaPic30.jpg'),
     pic22: require('../images/miaPic22.jpg'),
-    pic28: require('../images/miaPic28.jpg'),    
+    pic28: require('../images/miaPic28.jpg'),
     bookAppt: require('../images/bookappt.jpg')
 };
 
 function BodyComponent() {
-        return (
-            <Container fluid>
-                <Row style={{justifyContent: "center"}}>
-                    <Col lg={6} md={12} sm={12} xs={12}>
-                        <Image src={galpics.serveArea}  id="heightpic" alt="bookingpic" />
-                    </Col>
-                    <Col lg={6} md={12} sm={12} xs={12} style={{ paddingTop: "15px" }}>
-                        <Carousel fade id="carouselborder" slide interval={4500}>
-                            <Carousel.Item>
-                                <Image src={galpics.pic12} alt="First slide" fluid />
-                                <Carousel.Caption style={capstyle}>Chelley - "She is outstanding! Has patience and does a wonderful job!!"</Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <Image src={galpics.pic6} alt="Second slide" fluid />
-                                <Carousel.Caption style={capstyle}>Patti - "Maria does awesome nails"</Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <Image src={galpics.pic28} alt="Third slide" fluid />
-                                <Carousel.Caption style={capstyle}>Katrina - "She is awesome!!! Do not hesitate to hit her up!!"</Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <Image src={galpics.pic4} alt="Fourth slide" fluid />
-                                <Carousel.Caption style={capstyle}> Dianne - "She is absolutely amazing, takes her time and makes
+    return (
+        <Container fluid>
+            <Row style={{ justifyContent: "center" }}>
+                <Col lg={6} md={12} sm={12} xs={12}>
+                    <Image src={galpics.serveArea} id="heightpic" alt="bookingpic" />
+                </Col>
+                <Col lg={6} md={12} sm={12} xs={12} style={{ paddingTop: "15px" }}>
+                    <Carousel fade id="carouselborder" slide interval={4500}>
+                        <Carousel.Item>
+                            <Image src={galpics.pic12} alt="First slide" fluid />
+                            <Carousel.Caption style={capstyle}>Chelley - "She is outstanding! Has patience and does a wonderful job!!"</Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image src={galpics.pic6} alt="Second slide" fluid />
+                            <Carousel.Caption style={capstyle}>Patti - "Maria does awesome nails"</Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image src={galpics.pic28} alt="Third slide" fluid />
+                            <Carousel.Caption style={capstyle}>Katrina - "She is awesome!!! Do not hesitate to hit her up!!"</Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image src={galpics.pic4} alt="Fourth slide" fluid />
+                            <Carousel.Caption style={capstyle}> Dianne - "She is absolutely amazing, takes her time and makes
                                      sure you love it before you leave! She’s definitely my nail tech from here on out!"</Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <Image src={galpics.pic22} alt="Fifth slide" fluid />
-                                <Carousel.Caption style={capstyle}> Karen - "She does a wonderful job"</Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <Image src={galpics.bookAppt} alt="Sixth slide" fluid />
-                              </Carousel.Item>
-                        </Carousel>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col lg={12} md={12} sm={12} style={{ paddingTop: "19px"}}>
-                    <Table id="prices" style={{fontFamily: 'Manrope'}} size='md'
-                         responsive striped bordered hover
-                          variant="dark">
-                            <thead style={{ color: "pink", fontWeight: "bold" }}>
-                                <tr style={{ textAlign: "center" }}>
-                                    <th><Image id="thumbnailPic" src={galpics.tpic} thumbnail fluid/>
-                                    <br />Type of Set</th>
-                                    <th><Image id="thumbnailPic" src={galpics.shortNails} thumbnail fluid />
-                                    <br />Short Nails</th>
-                                    <th><Image id="thumbnailPic" src={galpics.medNails} thumbnail fluid />
-                                    <br />Med Nails </th>
-                                    <th><Image id="thumbnailPic" src={galpics.longNails} thumbnail fluid />
-                                    <br />Longs Nails</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td style={tableBody}>Acrylic Full Set:</td>
-                                    <td style={textCenter}>$ 35</td>
-                                    <td style={textCenter}>$ 45</td>
-                                    <td style={textCenter}>$ 50</td>
-                                </tr>
-                                <tr>
-                                    <td style={tableBody}>Acrylic Fill:</td>
-                                    <td style={textCenter}>$ 30</td>
-                                    <td style={textCenter}>$ 35</td>
-                                    <td style={textCenter}>$ 40</td>
-                                </tr>
-                                <tr>
-                                    <td style={tableBody}>Ombre Full Set:</td>
-                                    <td style={textCenter}>$ 40</td>
-                                    <td style={textCenter}>$ 55</td>
-                                    <td style={textCenter}>$ 55</td>
-                                </tr>
-                                <tr>
-                                    <td style={tableBody}>Ombre Fill:</td>
-                                    <td style={textCenter}>$ 35</td>
-                                    <td style={textCenter}>$ 40</td>
-                                    <td style={textCenter}>$ 40</td>
-                                </tr>
-                                <tr>
-                                    <td style={tableBody}>Color Acrylic Full Set:</td>
-                                    <td colSpan="3" style={{ textAlign: "center" }}>
-                                    **Nails need to be soaked off for an additional $ 5 (Full Set: $7)**
-                                    
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "violet", fontWeight: "bold", 
-                                    textShadow: "2px 2px 2px #f1069f" }}>Add-Ons:</td>
-                                    <td style={{ textAlign: "center" }}>* Jewel on 2 Fingers: $4 *</td>
-                                    <td style={{ textAlign: "center" }}>* Full Blinged out nail: $7 *</td>
-                                    <td style={{ textAlign: "center" }}>* Encapsulated Design: $3 (2 Fingers)  *</td>
-                                </tr>
-                                <tr>
-                                    <td style={{ color: "violet", fontWeight: "bold",
-                                     textShadow: "2px 2px 2px #f1069f" }}>Toe Color Change:</td>
-                                    <td colSpan="3" style={{ textAlign: "center" }}>$10 for Gel; $7 for regular</td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                     </Col>
-                </Row>
-            </Container>
-        );
-    }
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image src={galpics.pic22} alt="Fifth slide" fluid />
+                            <Carousel.Caption style={capstyle}> Karen - "She does a wonderful job"</Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image src={galpics.bookAppt} alt="Sixth slide" fluid />
+                        </Carousel.Item>
+                    </Carousel>
+                </Col>
+            </Row>
+            <Row>
+                <Col lg={12} md={12} sm={12} style={{ paddingTop: "19px" }}>
+                    <TableComponent />
+                </Col>
+            </Row>
+        </Container>
+    );
+}
 export default BodyComponent;
--- src/components/JumboComponent.js
+++ src/components/JumboComponent.js
@@ -20,71 +20,74 @@ const pics = {
 //     textShadow: "2px 2px 2px #f1069f",
 // }
 
-function JumboComponent () {
-   return (
-            <React.Fragment>
-                <Jumbotron fluid>
-                    <Container fluid>
-                        <Row style={{ textAlign: 'center', flexDirection: "row" }}>
-                            <Col xs={0} md={4} lg={4}>
-                                <Image id="jpic" style={{ height: "200px" }}
-                                    src={pics.miaPic12} alt="jpic1" roundedCircle />
-                            </Col>
-                            <Col xs={12} md={4} lg={4}>
-                                <Image id="jpic1" style={{ width: "250px", height: "270px" }}
-                                    src={pics.logo} alt="logopic" roundedCircle />
-                            </Col>
-                            <Col xs={0} md={4} lg={4}>
-                                <Image id="jpic" style={{ height: "200px" }}
-                                    src={pics.miaPic11} alt="jpic2" roundedCircle />
-                            </Col>
-                        </Row>
-                    </Container>
-                </Jumbotron>
-                <Navbar light collapseOnSelect expand="md" sticky="top">
-                    <Navbar.Brand style={{
-                        color: "#9df5cc",
-                        background: "black",
-                        padding: "11px",
-                        border: "2px solid #f1069f",
-                        borderRadius: "50%"
-                    }} href="#home">PBM</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="#prices" style={{ color: "#9df5cc" }}>Pricing</Nav.Link>
-                            <NavDropdown title={<span style={{ color: "#9df5cc" }}>Social Media</span>} id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1"
-                                    style={{ border: "5px solid #f1069f" }}>
-                                    <a role="button" className="btn btn-link" target="_blank"
-                                        rel="noreferrer noopener" href="tel:+17579127833">
-                                        <i id="socialIcon" className="fa fa-phone fa-lg" />
-                                    </a>  Questions?
+function JumboComponent() {
+    return (
+        <React.Fragment>
+            <Jumbotron fluid>
+                <Container fluid>
+                    <Row style={{ textAlign: 'center', flexDirection: "row" }}>
+                        <Col xs={0} md={4} lg={4}>
+                            <Image id="jpic" style={{ height: "200px" }}
+                                src={pics.miaPic12} alt="jpic1" roundedCircle />
+                        </Col>
+                        <Col xs={12} md={4} lg={4}>
+                            <Image id="jpic1" style={{ width: "250px", height: "270px" }}
+                                src={pics.logo} alt="logopic" roundedCircle />
+                        </Col>
+                        <Col xs={0} md={4} lg={4}>
+                            <Image id="jpic" style={{ height: "200px" }}
+                                src={pics.miaPic11} alt="jpic2" roundedCircle />
+                        </Col>
+                    </Row>
+                </Container>
+            </Jumbotron>
+            <Navbar light collapseOnSelect expand="md" sticky="top">
+                <Navbar.Brand style={{
+                    color: "#9df5cc",
+                    background: "black",
+                    padding: "11px",
+                    border: "2px solid #f1069f",
+                    borderRadius: "50%"
+                }} href="#home">PBM</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#prices" style={{ color: "#9df5cc" }}>Pricing</Nav.Link>
+                        <NavDropdown title={<span style={{ color: "#9df5cc" }}>Social Media</span>} id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1"
+                                style={{ border: "5px solid #f1069f" }}>
+                                <a role="button" className="btn btn-link" target="_blank"
+                                    rel="noreferrer noopener" href="tel:+17579127833">
+                                    <i id="socialIcon" className="fa fa-phone fa-lg" />
+                                </a>  Questions?
                                 </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2"
-                                    style={{ border: "5px solid #9df5cc" }}>
-                                    <a role="button" className="btn btn-link btn-instagram" target="_blank"
-                                        rel="noreferrer noopener" aria-hidden="true"
-                                        href="http://instagram.com/polished.bymia/">
-                                        <i id="socialIcon" className="fa fa-instagram fa-lg" />
-                                    </a>
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3"
-                                    style={{ border: "5px solid #f1069f" }}>
-                                    <a role="button" className="btn btn-link btn-facebook" target="_blank"
-                                        rel="noreferrer noopener" aria-hidden="true"
-                                        href="http://facebook.com/polishedbymia/">
-                                        <i id="socialIcon" className="fa fa-facebook fa-lg" />
-                                    </a>
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                        <Nav>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            </React.Fragment>
-        );
-    }
+                            <NavDropdown.Item href="#action/3.2"
+                                style={{ border: "5px solid #9df5cc" }}>
+                                <a role="button" className="btn btn-link btn-instagram" target="_blank"
+                                    rel="noreferrer noopener" aria-hidden="true"
+                                    href="http://instagram.com/polished.bymia/">
+                                    <i id="socialIcon" className="fa fa-instagram fa-lg" />
+                                </a> Instagram
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3"
+                                style={{ border: "5px solid #f1069f" }}>
+                                <a role="button" className="btn btn-link btn-facebook" target="_blank"
+                                    rel="noreferrer noopener" aria-hidden="true"
+                                    href="http://facebook.com/polishedbymia/">
+                                    <i id="socialIcon" className="fa fa-facebook fa-lg" />
+                                </a> facebook
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav.Item>
+                        Acrilic Designs: Full Sets and Cleanings
+                    </Nav.Item>
+                    <Nav>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </React.Fragment>
+    );
+}
 
 export default JumboComponent;

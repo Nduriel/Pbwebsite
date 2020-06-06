d2a1a670098f20a18a2227a4ea8f3928d3604e9f
Jean-Eric Pierre-Louis <55325769+Nduriel@users.noreply.github.com>
2020-06-06 12:46:37 -0400

    From REact Boot to Reactstarp

--- src/components/JumboComponent.js
+++ src/components/JumboComponent.js
@@ -1,45 +1,53 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Button } from 'reactstrap';
+import React, { useState } from 'react';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Nav from 'react-bootstrap/Nav';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+import {
+    Button, Navbar, Nav, NavbarToggler,
+    UncontrolledDropdown, DropdownToggle, Collapse,
+    NavLink, NavbarBrand,NavItem, DropdownItem, DropdownMenu,
+} from 'reactstrap';
 
 
 function JumboComponent(props) {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
     return (
         <>
             <Navbar light collapseOnSelect expand="md" sticky="top">
-                <Navbar.Brand style={{
+                <NavbarBrand style={{
                     color: "#9df5cc",
                     background: "black",
                     padding: "11px",
                     border: "2px solid #f1069f",
                     borderRadius: "50%"
-                }} href="#home">PBM</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Item className="glow">
-                            <Nav.Link href="/home" style={{ color: "#9df5cc" }}>Home</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item className="glow">
-                            <Nav.Link href="/prices" style={{ color: "#9df5cc" }}>Prices</Nav.Link>
-                        </Nav.Item>
-                        <NavDropdown className="glow" title={<span style={{ color: "#9df5cc" }}>Social Media</span>} id="collasible-nav-dropdown">
-                            <Nav.Item className="text-center">
+                }} href="#home">PBM</NavbarBrand>
+                <NavbarToggler onClick={toggle} aria-controls="responsive-navbar-nav" />
+                <Collapse isOpen={isOpen} navbar id="responsive-navbar-nav">
+                    <Nav className="mr-auto" navbar>
+                        <NavItem className="glow">
+                            <NavLink className="nav-link" href="/home" style={{ color: "#9df5cc" }}>Home</NavLink>
+                        </NavItem>
+                        <NavItem className="glow">
+                            <NavLink className="nav-link" href="/prices" style={{ color: "#9df5cc" }}>Prices</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown className="glow">
+                            <DropdownToggle nav caret style={{color: "#9df5cc"}}>
                                 Follow Me:
-                            </Nav.Item>
-                            <NavDropdown.Item href="http://instagram.com/polished.bymia/"
-                                target="_blank" rel="noreferrer noopener" aria-hidden="true">
-                                {/* <i id="socialIcon" className="fa fa-instagram fa-md" />{' '} */}
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem href="http://instagram.com/polished.bymia/"
+                                    target="_blank" rel="noreferrer noopener" aria-hidden="true">
+                                    {/* <i id="socialIcon" className="fa fa-instagram fa-md" />{' '} */}
                                 #Instagram
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="http://facebook.com/polishedbymia/"
-                                target="_blank" rel="noreferrer noopener" aria-hidden="true">
-                                {/* <i id="socialIcon" className="fa fa-facebook fa-md" />{' '} */}
+                            </DropdownItem>
+                                <DropdownItem href="http://facebook.com/polishedbymia/"
+                                    target="_blank" rel="noreferrer noopener" aria-hidden="true">
+                                    {/* <i id="socialIcon" className="fa fa-facebook fa-md" />{' '} */}
                                 @Facebook
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                            </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
                     </Nav>
                     <Nav className="ml-auto">
                         <Button color="primary" onClick={props.call}
@@ -55,7 +63,7 @@ function JumboComponent(props) {
                     </Nav>
                     <Nav>
                     </Nav>
-                </Navbar.Collapse>
+                </Collapse>
             </Navbar>
         </>
     );

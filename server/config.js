6c270acd5b4b1b477e9cbb60e858b9d83bcba617
Jean-Eric Pierre-Louis <55325769+Nduriel@users.noreply.github.com>
2020-07-07 16:31:39 -0400

    Nodemailer Trial 3

--- server/config.js
+++ server/config.js
@@ -1,10 +1,5 @@
-const nodemailer = require('nodemailer');
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'aros0885@gmail.com', // your email address to send email from
-    pass: 'wqqkvudujclwvnqd' // your gmail account password
-  }
-});
 
-module.exports = transporter;
+module.exports = {
+  USER: 'aros0885@gmail.com', 
+  PASS: 'wqqkvudujclwvnqd'
+}
--- server/index.js
+++ server/index.js
@@ -1,44 +1,55 @@
-const path = require('path');
-const express = require('express');
-const transporter = require('./config');
-const dotenv = require('dotenv');
-dotenv.config();
-const app = express();
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+var cors = require('cors');
+const creds = require('./config');
 
-const buildPath = path.join(__dirname, '..', 'build');
-app.use(express.json());
-app.use(express.static(buildPath));
-
-app.post('/send', (req, res) => {
-  try {
-    const mailOptions = {
-      from: req.body.email,
-      to: process.env.email,
-      subject: req.body.subject,
-      html: req.body.message
-    };
-
-    transporter.sendMail(mailOptions, function(err, info) {
-      if (err) {
-        res.status(500).send({
-          success: false,
-          message: 'Something went wrong. Try again later'
-        });
-      } else {
-        res.send({
-          success: true,
-          message: 'Thanks for contacting us. We will get back to you shortly'
-        });
-      }
-    });
-  } catch (error) {
-    res.status(500).send({
-      success: false,
-      message: 'Something went wrong. Try again later'
-    });
+var transport = {
+    host: 'gmail.com', // Don’t forget to replace with the SMTP host of your provider
+    port: 587,
+    auth: {
+    user: creds.USER,
+    pass: creds.PASS
   }
-});
+}
 
-app.listen(3030, () => {
-  console.log('server start on port 3030');
-});
+var transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Server is ready to take messages');
+    }
+  });
+  
+  router.post('/send', (req, res, next) => {
+    var firstName = req.body.firstName
+    var email = req.body.email
+    var content = `firstName: ${firstName} \n email: ${email} \n  `
+  
+    var mail = {
+      from: firstName,
+      to: 'aros0885@gmail.com',  // Change to email address that you want to receive messages on
+      subject: 'New Message from Contact Form',
+      text: content
+    }
+  
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        res.json({
+          status: 'fail'
+        })
+      } else {
+        res.json({
+         status: 'success'
+        })
+      }
+    })
+  })
+  
+  const app = express()
+  app.use(cors())
+  app.use(express.json())
+  app.use('/', router)
+  app.listen(3002)

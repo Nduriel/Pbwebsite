384b38a39811b880a9ab0c0c679f2bb08399891d
Jean-Eric Pierre-Louis <55325769+Nduriel@users.noreply.github.com>
2020-07-09 12:04:43 -0400

    NavLink

--- server/index.js
+++ server/index.js
@@ -1,55 +1,55 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var nodemailer = require('nodemailer');
-var cors = require('cors');
-const creds = require('./config');
+var nodemailer = require("nodemailer");
+var cors = require("cors");
+const creds = require("./config");
 
 var transport = {
-    host: 'gmail.com', // Don’t forget to replace with the SMTP host of your provider
-    port: 587,
-    auth: {
+  host: "gmail.com", // Don’t forget to replace with the SMTP host of your provider
+  port: 587,
+  auth: {
     user: creds.USER,
-    pass: creds.PASS
-  }
-}
+    pass: creds.PASS,
+  },
+};
 
-var transporter = nodemailer.createTransport(transport)
+var transporter = nodemailer.createTransport(transport);
 
 transporter.verify((error, success) => {
-    if (error) {
-      console.log(error);
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take messages");
+  }
+});
+
+router.post("/send", (req, res, next) => {
+  var firstName = req.body.firstName;
+  var email = req.body.email;
+  var content = `firstName: ${firstName} \n email: ${email} \n  `;
+
+  var mail = {
+    from: firstName,
+    to: "aros0885@gmail.com", // Change to email address that you want to receive messages on
+    subject: "New Message from Contact Form",
+    text: content,
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: "fail",
+      });
     } else {
-      console.log('Server is ready to take messages');
+      res.json({
+        status: "success",
+      });
     }
   });
-  
-  router.post('/send', (req, res, next) => {
-    var firstName = req.body.firstName
-    var email = req.body.email
-    var content = `firstName: ${firstName} \n email: ${email} \n  `
-  
-    var mail = {
-      from: firstName,
-      to: 'aros0885@gmail.com',  // Change to email address that you want to receive messages on
-      subject: 'New Message from Contact Form',
-      text: content
-    }
-  
-    transporter.sendMail(mail, (err, data) => {
-      if (err) {
-        res.json({
-          status: 'fail'
-        })
-      } else {
-        res.json({
-         status: 'success'
-        })
-      }
-    })
-  })
-  
-  const app = express()
-  app.use(cors())
-  app.use(express.json())
-  app.use('/', router)
-  app.listen(3002)
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(3002);
--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -1,134 +1,140 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-    Button, Navbar, Nav, NavbarToggler,
-    UncontrolledDropdown, DropdownToggle, Collapse,
-    NavLink, NavbarBrand, NavItem, DropdownItem, DropdownMenu,
-} from 'reactstrap';
-import ModalComponent from './ModalComponent';
-
+  Button,
+  Navbar,
+  Nav,
+  NavbarToggler,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Collapse,
+  NavbarBrand,
+  NavItem,
+  DropdownItem,
+  DropdownMenu,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+import ModalComponent from "./ModalComponent";
 
 function NavBarComponent() {
+  //Modal Toggler
+  const [isModalOpen, setModal] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleModal = () => {
+    setModal(!isModalOpen);
+  };
 
-    //Modal Toggler
-    const [isModalOpen, setModal] = useState(false);
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleModal = () => {
-        setModal(!isModalOpen);
-    };
+  //Navbar Toggler
+  const toggle = () => setIsOpen(!isOpen);
 
-    //Navbar Toggler
-    const toggle = () => setIsOpen(!isOpen);
+  const buttonStyle = {
+    fontWeight: "bold",
+    fontSize: "18px",
+    boxShadow: "2px 2px 2px #f1069f",
+    textShadow: "2px 2px 2px #f1069f",
+    margin: "10px",
+    borderRadius: "5%",
+  };
 
-    const buttonStyle = {
-        fontWeight: "bold",
-        fontSize: "18px",
-        boxShadow: "2px 2px 2px #f1069f",
-        textShadow: "2px 2px 2px #f1069f",
-        margin: "10px",
-        borderRadius: "5%",
-    }
+  const navbarIcon = {
+    color: "#9df5cc",
+    background: "black",
+    padding: "11px",
+    border: "3px solid #f1069f",
+    borderRadius: "50% / 50%",
+  };
 
-    const navbarIcon = {
-        color: "#9df5cc",
-        background: "black",
-        padding: "11px",
-        border: "3px solid #f1069f",
-        borderRadius: "50% / 50%",
-    }
+  const activeStyle = {
+    color: "white",
+  };
 
-    return (
-        <>
-            <ModalComponent
-                toggleModal={toggleModal}
-                isModalOpen={isModalOpen}
-            />
-            <Navbar light
-                collapseOnSelect
-                expand="md"
-                classticky="top"
-            >
-                <NavbarToggler
-                    onClick={toggle}
-                    aria-controls="responsive-navbar-nav"
-                />
-                <Collapse
-                    isOpen={isOpen}
-                    navbar id="responsive-navbar-nav"
+  return (
+    <>
+      <ModalComponent toggleModal={toggleModal} isModalOpen={isModalOpen} />
+      <Navbar light collapseOnSelect expand="md" classticky="top">
+        <NavbarToggler onClick={toggle} aria-controls="responsive-navbar-nav" />
+        <Collapse isOpen={isOpen} navbar id="responsive-navbar-nav">
+          <Nav className="mr-auto" navbar>
+            <NavItem className="glow">
+              <NavLink
+                className="nav-link"
+                activeStyle={activeStyle}
+                to="/home"
+                style={{
+                  color: "#9df5cc",
+                }}
+              >
+                Home
+              </NavLink>
+            </NavItem>
+            <NavItem className="glow">
+              <NavLink
+                className="nav-link"
+                activeStyle={activeStyle}
+                to="/prices"
+                style={{
+                  color: "#9df5cc",
+                }}
+              >
+                Prices
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown className="glow">
+              <DropdownToggle nav caret style={{ color: "#9df5cc" }}>
+                Social
+              </DropdownToggle>
+              <DropdownMenu
+                right
+                style={{
+                  border: "3px solid purple",
+                  background: "black",
+                  color: "white",
+                }}
+              >
+                <DropdownItem
+                  // {  color: "white", background: "#bc2a8d" }
+                  href="http://instagram.com/polished.bymia/"
+                  id="dropIG"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  aria-hidden="true"
                 >
-                    <Nav className="mr-auto" navbar>
-                        <NavItem className="glow">
-                            <NavLink
-                                className="nav-link"
-                                href="/home"
-                                style={{
-                                    color: "#9df5cc"
-                                }}>
-                                Home
-                            </NavLink>
-                        </NavItem>
-                        <NavItem className="glow">
-                            <NavLink
-                                className="nav-link"
-                                href="/prices"
-                                style={{
-                                    color: "#9df5cc"
-                                }}>
-                                Prices
-                            </NavLink>
-                        </NavItem>
-                        <UncontrolledDropdown className="glow">
-                            <DropdownToggle
-                                nav caret
-                                style={{ color: "#9df5cc" }}>
-                                Social
-                            </DropdownToggle>
-                            <DropdownMenu right
-                                style={{
-                                    border: "3px solid purple",
-                                    background: "black",
-                                    color: "white"
-                                }}>
-                                <DropdownItem
-                                    // {  color: "white", background: "#bc2a8d" }
-                                    href="http://instagram.com/polished.bymia/"
-                                    id="dropIG"
-                                    target="_blank"
-                                    rel="noreferrer noopener"
-                                    aria-hidden="true">
-                                    <i className="fa fa-instagram fa-lg" />{' '}
-                                #Instagram
-                            </DropdownItem>
-                                <DropdownItem href="http://facebook.com/polishedbymia/"
-                                    style={{
-                                        color: "white",
-                                        background: "#007aff"
-                                    }}
-                                    target="_blank"
-                                    rel="noreferrer noopener"
-                                    aria-hidden="true">
-                                    <i className="fa fa-facebook fa-lg" />{' '}
-                                @Facebook
-                            </DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </Nav>
-                    <Nav>
-                        <NavbarBrand className="mr-5" style={navbarIcon} href="#home">
-                            PBM
-                        </NavbarBrand></Nav>
-                    <Nav className="ml-auto">
-                        <Button color="primary" onClick={toggleModal}
-                            size="lg"
-                            style={buttonStyle}>
-                            Appointments
-                        </Button>
-                    </Nav>
-                    <Nav>
-                    </Nav>
-                </Collapse>
-            </Navbar>
-        </>
-    );
+                  <i className="fa fa-instagram fa-lg" /> #Instagram
+                </DropdownItem>
+                <DropdownItem
+                  href="http://facebook.com/polishedbymia/"
+                  style={{
+                    color: "white",
+                    background: "#007aff",
+                  }}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  aria-hidden="true"
+                >
+                  <i className="fa fa-facebook fa-lg" /> @Facebook
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <Nav>
+            <NavbarBrand className="mr-5" style={navbarIcon} href="#home">
+              PBM
+            </NavbarBrand>
+          </Nav>
+          <Nav className="ml-auto">
+            <Button
+              color="primary"
+              onClick={toggleModal}
+              size="lg"
+              style={buttonStyle}
+            >
+              Appointments
+            </Button>
+          </Nav>
+          <Nav></Nav>
+        </Collapse>
+      </Navbar>
+    </>
+  );
 }
 
 export default NavBarComponent;

8c608bd8c3f9a28ffc6834061d4eb35cf08ba4f6
Jean-Eric Pierre-Louis <55325769+Nduriel@users.noreply.github.com>
2020-08-05 20:45:11 -0400

    Footer Change

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -12,64 +12,62 @@ const footerFont = {
 function FooterComponent() {
   return (
     <footer className="site-footer" style={footerFont}>
-      <Container fluid>
-        <Row>
-          <Col
-            className="text-center"
-            id="goneText"
-            lg={4}
-            style={{
-              paddingTop: "15px",
-              borderRight: "3px outset pink",
-            }}
+      <Row>
+        <Col
+          className="text-center"
+          id="goneText"
+          lg={4}
+          style={{
+            paddingTop: "15px",
+            borderRight: "3px outset pink",
+          }}
+        >
+          "Modern touch, Beautiful feel"
+        </Col>
+        <Col className="text-center" lg={4} md={12} sm={12}>
+          <a
+            role="button"
+            className="btn btn-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="mailto:Mmnuoz214@gmail.com"
           >
-            "Modern touch, Beautiful feel"
-          </Col>
-          <Col className="text-center" lg={4} md={12} sm={12}>
-            <a
-              role="button"
-              className="btn btn-link"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="mailto:Mmnuoz214@gmail.com"
-            >
-              <i id="socialIcon" className="fa fa-envelope" />
-            </a>
-            <span id="goneText">Questions? Comments?</span>
-            <a
-              role="button"
-              className="btn btn-link"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="tel:+17579127833"
-            >
-              <i role="button" id="socialIcon" className="fa fa-phone fa-lg" />
-            </a>
-          </Col>
+            <i id="socialIcon" className="fa fa-envelope" />
+          </a>
+          <span id="goneText">Questions? Comments?</span>
+          <a
+            role="button"
+            className="btn btn-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="tel:+17579127833"
+          >
+            <i role="button" id="socialIcon" className="fa fa-phone fa-lg" />
+          </a>
+        </Col>
 
-          <Col
-            lg={4}
-            id="goneText"
-            className="text-center"
-            style={{
-              paddingTop: "15px",
-              borderLeft: "3px outset pink",
-            }}
-          >
-            <span>
-              <ul className="list-unstyled">
-                <li>
-                  <i className="fa fa-copyright" aria-hidden="true" />
-                  <i>PolishedByMia</i>
-                </li>
-                <li>
-                  <i>Liscensed Nail Technician</i>
-                </li>
-              </ul>
-            </span>
-          </Col>
-        </Row>
-      </Container>
+        <Col
+          lg={4}
+          id="goneText"
+          className="text-center"
+          style={{
+            paddingTop: "15px",
+            borderLeft: "3px outset pink",
+          }}
+        >
+          <span>
+            <ul className="list-unstyled">
+              <li>
+                <i className="fa fa-copyright" aria-hidden="true" />
+                <i>PolishedByMia</i>
+              </li>
+              <li>
+                <i>Liscensed Nail Technician</i>
+              </li>
+            </ul>
+          </span>
+        </Col>
+      </Row>
     </footer>
   );
 }

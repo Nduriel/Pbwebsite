af73807212a09f388c52517829008a362a4874da
Jean-Eric Pierre-Louis <55325769+Nduriel@users.noreply.github.com>
2020-09-29 16:51:19 -0400

    Policy Css

--- src/components/policies.js
+++ src/components/policies.js
@@ -2,77 +2,103 @@ import React from "react";
 
 export const policies = (
   <p>
-    <span style={{ textShadow: "1px 2px 2px blue" }}>Polished by Mia</span>{" "}
+    <span className="policy">
+      <i>Polished by Mia</i>
+    </span>{" "}
     Policies: The purpose of this policy is to advise you as our customer, as to
     the services that can be expected from Polished by Mia, and the steps and
     procedure that can be taken if you, the customer are not satisfied with the
-    services you receive from Polished by Mia. What we offer: Manicure &
-    Pedicure Artificial Nails Nail art Payment Methods:{" "}
+    services you receive from Polished by Mia. <br />
+    <ul>
+      <span className="policy">What we offer:</span>
+      <li id="policyList">Manicure & Pedicure </li>
+      <li id="policyList">Artificial Nails</li>
+      <li id="policyList">Nail Art</li>
+    </ul>
+    <span className="policy">Payment Methods:</span>{" "}
     <span style={{ textShadow: "1px 2px 2px red" }}>
-      Cash, Cash App, Apple Pay and Gift Certificates are acceptable tenders for
-      all services and products
-    </span>
-    . Deposits: Polished by Mia is primarily a “by appointment” business,
-    therefore, last minute cancellations can have a great impact to our business
-    and that of our technician. Because of this{" "}
-    <u style={{ textShadow: "1px 2px 2px green" }}>
+      Cash, Cash App, Apple Pay and Gift Certificates
+    </span>{" "}
+    are acceptable tenders for all services and products.
+    <br />
+    <span className="policy"> Deposits:</span> Polished by Mia is primarily a
+    “by appointment” business, therefore, last minute cancellations can have a
+    great impact to our business and that of our technician. Because of this{" "}
+    <i style={{ textShadow: "1px 2px 2px red" }}>
       deposits are required as of 10/01/2020.
-    </u>{" "}
+    </i>{" "}
     A $20 deposit can be made by cash, cash app or apple pay and the amount
     charged will be applied towards the service on the day of your appointment.
-    Lost or stolen Gift Cards: Treat your Gift Card like cash. Lost or stolen
-    cards will not be replaced or refunded. Gift Cards cannot be canceled: You
-    cannot cancel your Gift Card. Cancelations: Appointments can be cancelled 24
-    hours in advance by calling, texting, or messaging Polished by Mia so that
-    your reserved time can be given to another client on our waiting list. We
-    understand that sometimes emergencies happen, and it is not always possible
-    to give adequate notice when you cannot make an appointment. However, be
-    aware that if you repeatedly cancel appointments without proper notice, we
-    reserve the right to ask for a non-refundable deposit to secure your next
-    appointment.{" "}
+    <br />
+    <span className="policy">Lost or stolen Gift Cards:</span>
+    Treat your Gift Card like cash. Lost or stolen cards will not be replaced or
+    refunded.
+    <br />
+    <span className="policy">Gift Cards cannot be canceled:</span>
+    You cannot cancel your Gift Card.
+    <br />
+    <span className="policy">Cancelations:</span> Appointments can be cancelled
+    24 hours in advance by calling, texting, or messaging Polished by Mia so
+    that your reserved time can be given to another client on our waiting list.
+    We understand that sometimes emergencies happen, and it is not always
+    possible to give adequate notice when you cannot make an appointment.
+    However, be aware that if you repeatedly cancel appointments without proper
+    notice, we reserve the right to ask for a non-refundable deposit to secure
+    your next appointment.
+    <br />
     <span style={{ textShadow: "1px 2px 2px purple" }}>
-      Late Arrivals: Tardiness Scheduled appointments have a 15-minute grace
-      period to allow for unpredicted traffic or parking difficulties.
+      Late Arrivals:{" "}
     </span>{" "}
-    If you anticipate that you will be later than 15 minutes, please call ahead
-    to see if we have the availability to complete your service in full. One
-    late client can throw off our entire schedule and cause other clients to be
-    late for their commitments. Therefore, if you arrive more than 15 minutes
-    late, we reserve the right to refuse partial or complete services.{" "}
+    Tardiness Scheduled appointments have a 15-minute grace period to allow for
+    unpredicted traffic or parking difficulties. If you anticipate that you will
+    be later than 15 minutes, please call ahead to see if we have the
+    availability to complete your service in full. One late client can throw off
+    our entire schedule and cause other clients to be late for their
+    commitments. Therefore, if you arrive more than 15 minutes late, we reserve
+    the right to refuse partial or complete services. <br />
     <span style={{ textShadow: "1px 2px 2px purple" }}>
-      No Call No Show: If you do not arrive to your scheduled appointment time
-      within 15-minutes you will be considered a no-show, which will result in a
-      $25 No Show Fee, this No-Show Fee will be due within 24-hours of your
-      scheduled appointment time via $Cash App
+      No Call No Show:
     </span>{" "}
-    . *** Unforeseen circumstances may be exceptions to this policy! Please
-    speak with Polished by Mia directly to discuss. *** Children and Group
-    Policy: Due to hazardous chemicals, products and implements within the salon
-    environment, Polished by Mia asks that you please refrain from bringing
-    small children to your appointment! Polished by Mia is a small studio space
-    so please refrain from brining multiple friends/family or group of
-    friends/family to your appointments. Refunds: If you are not happy with your
-    nails, please make us aware of it before you pay. We will adjust them to
-    your satisfaction or remove any enhancements or coatings that have been
-    applied. No refunds will be given after you have left the salon.
-    Complimentary Fixes: If you are ever dissatisfied with any service in the
-    salon, we will gladly make the corrections necessary to exceed your
-    expectations within 3 days of your original services. No refund is given
-    after it has been rendered. – Enhancements and gel polish manicures are
-    guaranteed for 5 business days after your appointment; excluding breakages.
-    If you lose an enhancement or notice chips or lifting in the first 5 days,
-    please call us to schedule a free repair. (Repairs must be arranged within 3
-    days of notification to the salon to be considered complimentary fixes.)
-    -Traditional Nail polish services are not guaranteed. However we would be
-    glad to fix them for you. Must be called in within 24-48 hours after initial
-    service and schedule an appointment for nail fix within 2 days. Quality
-    Guarantee- Regular Polish: Regular polish applications may last in duration
-    from 1 to 7-days depending on several factors specific to the client,
-    including: client lifestyle, client health and following of maintenance
-    instructions. Polished by Mia does NOT guarantee the longevity of ANY
-    regular polish product once your appointment has been completed due to the
-    varying factors explained above, the product formulation and manufacturer’s
-    instructions on regular nail polish. Quality Guarantee-Gel Polish: Gel
+    If you do not arrive to your scheduled appointment time within 15-minutes
+    you will be considered a no-show, which will result in a $25 No Show Fee,
+    this No-Show Fee will be due within 24-hours of your scheduled appointment
+    time via $Cash App
+    <i>
+      . *** Unforeseen circumstances may be exceptions to this policy! Please
+      speak with Polished by Mia directly to discuss. ***
+    </i>
+    <br /> <span className="policy">Children and Group Policy: </span>Due to
+    hazardous chemicals, products and implements within the salon environment,
+    Polished by Mia asks that you please refrain from bringing small children to
+    your appointment! Polished by Mia is a small studio space so please refrain
+    from brining multiple friends/family or group of friends/family to your
+    appointments.
+    <br />
+    <span className="policy">Refunds:</span> If you are not happy with your
+    nails, please make us aware of it <u>before you pay</u>. We will adjust them
+    to your satisfaction or remove any enhancements or coatings that have been
+    applied. No refunds will be given after you have left the salon. <br />
+    <span className="policy">Complimentary Fixes:</span> If you are ever
+    dissatisfied with any service in the salon, we will gladly make the
+    corrections necessary to exceed your expectations within 3 days of your
+    original services. No refund is given after it has been rendered. <br />–
+    Enhancements and gel polish manicures are guaranteed for 5 business days
+    after your appointment; excluding breakages. If you lose an enhancement or
+    notice chips or lifting in the first 5 days, please call us to schedule a
+    free repair. (Repairs must be arranged within 3 days of notification to the
+    salon to be considered complimentary fixes.) <br />– Traditional Nail polish
+    services are <u>not</u> guaranteed. However we would be glad to fix them for
+    you. Must be called in within 24-48 hours after initial service and schedule
+    an appointment for nail fix within 2 days. <br />
+    <span className="policy">Quality Guarantee- Regular Polish:</span> Regular
+    polish applications may last in duration from 1 to 7-days depending on
+    several factors specific to the client, including: client lifestyle, client
+    health and following of maintenance instructions. Polished by Mia does NOT
+    guarantee the longevity of ANY regular polish product once your appointment
+    has been completed due to the varying factors explained above, the product
+    formulation and manufacturer’s instructions on regular nail polish.
+    <br />
+    <span className="policy">Quality Guarantee-Gel Polish: </span> Gel
     Polish—often referred to as “Shellac”—is a light-cured polish product that
     may last a duration of 2-4 weeks depending on several factors specific to
     the client, including: client lifestyle, client health and following of
@@ -84,29 +110,38 @@ export const policies = (
     salon to have a repair done FREE of charge. However, it is at Polished by
     Mia’s discretion! If we feel that your product breakdown was not caused by
     error in application and exceeds normal wear and tear you will be charged
-    for the nail repair service. Quality Guarantee- 3D Embellishments: All nail
-    art fixtures with raised surfaces such as Jewels, glitter and 3-D
+    for the nail repair service.
+    <br />
+    <span className="policy">Quality Guarantee- 3D Embellishments:</span> All
+    nail art fixtures with raised surfaces such as Jewels, glitter and 3-D
     embellishments are NOT guaranteed by Polished by Mia due to the
     unpredictable nature of the client’s daily activity that may cause loss of
-    these items. Nail Fill Policy: Maintenance should be performed on Nail
-    Enhancement services every 2 to 3--weeks to ensure the integrity of your
-    service and the health of your natural nails. Due to an increased product
-    cost and increased appointment time required for maintenance on Nail
-    Enhancements, those individuals who wish to receive or exceed a 4--week
-    maintenance schedule will be charged for a Full Set. *There may be
-    exceptions to this policy, as we understand that the rate of natural nail
-    growth may be different for every client. We ask that each client discuss
-    this with us so we can make appropriate appointment arrangements.{" "}
+    these items.
+    <br />
+    <span className="policy">Nail Fill Policy: </span> Maintenance should be
+    performed on Nail Enhancement services every 2 to 3--weeks to ensure the
+    integrity of your service and the health of your natural nails. Due to an
+    increased product cost and increased appointment time required for
+    maintenance on Nail Enhancements, those individuals who wish to receive or
+    exceed a 4--week maintenance schedule will be charged for a Full Set.
+    <br />
+    <i>
+      {" "}
+      *There may be exceptions to this policy, as we understand that the rate of
+      natural nail growth may be different for every client. We ask that each
+      client discuss this with us so we can make appropriate appointment
+      arrangements.
+    </i>{" "}
     <span style={{ textShadow: "1px 2px 2px red" }}>
       ** Please remember, nails are jewels, not tools. Be kind to your nails and
       they will look beautiful for weeks after your service. **
     </span>{" "}
-    We Reserve the Right to Refuse Service To: Anyone with a nail condition we
-    suspect may be contagious Anyone with open or infected wounds on the
-    treatment area Anyone in ill health that we may suspect may be contagious,
-    or we fear could be harmed by our service Anyone more than 10-15 minutes
-    late for an appointment Anyone demonstrating inappropriate behavior to our
-    nail technicians and other customers
+    <span className="policy">We Reserve the Right to Refuse Service To:</span>{" "}
+    Anyone with a nail condition we suspect may be contagious Anyone with open
+    or infected wounds on the treatment area Anyone in ill health that we may
+    suspect may be contagious, or we fear could be harmed by our service Anyone
+    more than 10-15 minutes late for an appointment Anyone demonstrating
+    inappropriate behavior to our nail technicians and other customers
   </p>
 );
 

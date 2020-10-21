1281e4784f4d1218881329f1e4e898968b8e9c09
Jean-Eric Pierre-Louis <55325769+Nduriel@users.noreply.github.com>
2020-10-21 12:46:49 -0400

    pics updated

--- src/components/photos.js
+++ src/components/photos.js
@@ -1,74 +1,105 @@
 export const carouselPic = [
+  // {
+  //   altText: "1",
+  //   src: require("../images/bookappt2.jpeg"),
+  // },
+  // {
+  //   altText: "2",
+  //   src: require("../images/pic1.jpg"),
+  //   caption: `#goodformnails - "Wow, these are perfect!"`,
+  // },
+  // {
+  //   altText: "3",
+  //   src: require("../images/pic2.JPG"),
+  //   caption:
+  //     "Chelley - She is outstanding! Has patience and does a wonderful job!!",
+  // },
+  // {
+  //   altText: "4",
+  //   src: require("../images/pic3.JPG"),
+  //   caption: `Patti - "Maria does awesome nails"`,
+  // },
+  // {
+  //   altText: "5",
+  //   src: require("../images/pic4.JPG"),
+  //   caption: `Stacey - I'm super picky with my nails, but she does an amazing job!!!`,
+  // },
+  // {
+  //   altText: "6",
+  //   src: require("../images/pic5.JPG"),
+  //   caption: `Dianne - "She is absolutely amazing, takes her time and makes
+  //       sure you love it before you leave! She’s definitely my nail tech from here on out!"`,
+  // },
+  // {
+  //   altText: "7",
+  //   src: require("../images/pic6.JPG"),
+  //   caption: `Karen - "She does a wonderful job"`,
+  // },
+  // {
+  //   altText: "8",
+  //   src: require("../images/pic7.JPG"),
+  //   caption: `Chelley - "She is outstanding! Has patience and does a wonderful job!!"`,
+  // },
+  // {
+  //   altText: "9",
+  //   src: require("../images/pic8.JPG"),
+  //   caption: `Katrina - "She is awesome!!! Do not hesitate to hit her up!!"`,
+  // },
+  // {
+  //   altText: "10",
+  //   src: require("../images/pic9.JPG"),
+  //   caption: `Follow on Facebook and Instagram!`,
+  // },
+  // {
+  //   altText: "12",
+  //   src: require("../images/pic11.JPG"),
+  //   caption: ``,
+  // },
+  // {
+  //   altText: "13",
+  //   src: require("../images/pic12.JPG"),
+  //   caption: `#757nails`,
+  // },
+  // {
+  //   altText: "14",
+  //   src: require("../images/pic13.JPG"),
+  //   caption: `#nailsofinstagram`,
+  // },
   {
-    altText: "1",
-    src: require("../images/bookappt2.jpeg"),
+    altText: "15",
+    src: require("../images/pic14.jpg"),
   },
   {
-    altText: "2",
-    src: require("../images/pic1.jpg"),
-    caption: `#goodformnails - "Wow, these are perfect!"`,
+    altText: "16",
+    src: require("../images/pic15.jpg"),
   },
   {
-    altText: "3",
-    src: require("../images/pic2.JPG"),
-    caption:
-      "Chelley - She is outstanding! Has patience and does a wonderful job!!",
+    altText: "17",
+    src: require("../images/pic16.jpg"),
   },
   {
-    altText: "4",
-    src: require("../images/pic3.JPG"),
-    caption: `Patti - "Maria does awesome nails"`,
+    altText: "18",
+    src: require("../images/pic17.jpg"),
   },
   {
-    altText: "5",
-    src: require("../images/pic4.JPG"),
-    caption: `Stacey - I'm super picky with my nails, but she does an amazing job!!!`,
+    altText: "19",
+    src: require("../images/pic18.jpg"),
   },
   {
-    altText: "6",
-    src: require("../images/pic5.JPG"),
-    caption: `Dianne - "She is absolutely amazing, takes her time and makes
-        sure you love it before you leave! She’s definitely my nail tech from here on out!"`,
+    altText: "20",
+    src: require("../images/pic19.jpg"),
   },
   {
-    altText: "7",
-    src: require("../images/pic6.JPG"),
-    caption: `Karen - "She does a wonderful job"`,
+    altText: "21",
+    src: require("../images/pic20.jpg"),
   },
   {
-    altText: "8",
-    src: require("../images/pic7.JPG"),
-    caption: `Chelley - "She is outstanding! Has patience and does a wonderful job!!"`,
+    altText: "22",
+    src: require("../images/pic21.jpg"),
   },
   {
-    altText: "9",
-    src: require("../images/pic8.JPG"),
-    caption: `Katrina - "She is awesome!!! Do not hesitate to hit her up!!"`,
-  },
-  {
-    altText: "10",
-    src: require("../images/pic9.JPG"),
-    caption: `Follow on Facebook and Instagram!`,
-  },
-  {
-    altText: "11",
-    src: require("../images/pic10.JPG"),
-    caption: `#polishedbymia`,
-  },
-  {
-    altText: "12",
-    src: require("../images/pic11.JPG"),
-    caption: ``,
-  },
-  {
-    altText: "13",
-    src: require("../images/pic12.JPG"),
-    caption: `#757nails`,
-  },
-  {
-    altText: "14",
-    src: require("../images/pic13.JPG"),
-    caption: `#nailsofinstagram`,
+    altText: "23",
+    src: require("../images/pic22.jpg"),
   },
 ];
 

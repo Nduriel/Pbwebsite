90842594f6007aee788d1454dd6928085caae546
Jean-Eric Pierre-Louis <55325769+Nduriel@users.noreply.github.com>
2020-10-22 14:16:32 -0400

    trigger

--- src/components/photos.js
+++ src/components/photos.js
@@ -6,38 +6,49 @@ export const carouselPic = [
   {
     altText: "2",
     src: require("../images/pic1.jpg"),
+    caption: `#goodformnails - "Wow, these are perfect!"`,
   },
   {
     altText: "3",
     src: require("../images/pic2.jpg"),
+    caption:
+      "Chelley - She is outstanding! Has patience and does a wonderful job!!",
   },
   {
     altText: "4",
     src: require("../images/pic3.jpg"),
+    caption: `#polishedbymia`,
   },
   {
     altText: "5",
     src: require("../images/pic4.jpg"),
+    caption: `Stacey - I'm super picky with my nails, but she does an amazing job!!!`,
   },
   {
     altText: "6",
     src: require("../images/pic5.jpg"),
+    caption: `Dianne - "She is absolutely amazing, takes her time and makes
+    sure you love it before you leave! She’s definitely my nail tech from here on out!"`,
   },
   {
     altText: "7",
     src: require("../images/pic6.jpg"),
+    caption: `Karen - "She does a wonderful job"`,
   },
   {
     altText: "8",
     src: require("../images/pic7.jpg"),
+    caption: `Chelley - "She is outstanding! Has patience and does a wonderful job!!"`,
   },
   {
     altText: "9",
     src: require("../images/pic8.jpg"),
+    caption: `Katrina - "She is awesome!!! Do not hesitate to hit her up!!"`,
   },
   {
     altText: "10",
     src: require("../images/pic9.jpg"),
+    caption: `Follow on Facebook and Instagram!`,
   },
 ];
 

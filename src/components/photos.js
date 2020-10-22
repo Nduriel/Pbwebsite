5986f4000f1ea1d56af855fd6e2809d38d41b14c
Jean-Eric Pierre-Louis <55325769+Nduriel@users.noreply.github.com>
2020-10-22 16:08:14 -0400

    trial changes

--- src/components/photos.js
+++ src/components/photos.js
@@ -1,52 +1,52 @@
 export const carouselPic = [
   {
     altText: "1",
-    src: require("../images/bookappt2.jpeg"),
+    src: require("../images/bookappt.jpeg"),
   },
   {
     altText: "2",
-    src: require("../images/pic1.jpg"),
+    src: require("../images/pic14.jpg"),
     caption: `#goodformnails - "Wow, these are perfect!"`,
   },
   {
     altText: "3",
-    src: require("../images/pic2.jpg"),
+    src: require("../images/pic15.jpg"),
     caption: `Chelley - She is outstanding! Has patience and does a wonderful job!!`,
   },
   {
     altText: "4",
-    src: require("../images/pic3.jpg"),
+    src: require("../images/pic16.jpg"),
     caption: `#polishedbymia`,
   },
   {
     altText: "5",
-    src: require("../images/pic4.jpg"),
+    src: require("../images/pic17.jpg"),
     caption: `Stacey - I'm super picky with my nails, but she does an amazing job!!!`,
   },
   {
     altText: "6",
-    src: require("../images/pic5.jpg"),
+    src: require("../images/pic18.jpg"),
     caption: `Dianne - "She is absolutely amazing, takes her time and makes
     sure you love it before you leave! She’s definitely my nail tech from here on out!"`,
   },
   {
     altText: "7",
-    src: require("../images/pic6.jpg"),
+    src: require("../images/pic19.jpg"),
     caption: `Karen - "She does a wonderful job"`,
   },
   {
     altText: "8",
-    src: require("../images/pic7.jpg"),
+    src: require("../images/pic20.jpg"),
     caption: `Chelley - "She is outstanding! Has patience and does a wonderful job!!"`,
   },
   {
     altText: "9",
-    src: require("../images/pic8.jpg"),
+    src: require("../images/pic21.jpg"),
     caption: `Katrina - "She is awesome!!! Do not hesitate to hit her up!!"`,
   },
   {
     altText: "10",
-    src: require("../images/pic9.jpg"),
+    src: require("../images/pic22.jpg"),
     caption: `Follow on Facebook and Instagram!`,
   },
 ];

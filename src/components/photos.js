0c162620940c7f64a105d99f2f81ae1be0bdc712
Jean-Eric Pierre-Louis <55325769+Nduriel@users.noreply.github.com>
2020-10-22 14:08:56 -0400

    fail 3

--- src/components/photos.js
+++ src/components/photos.js
@@ -1,42 +1,42 @@
 export const carouselPic = [
   {
-    altText: "bookingPic",
+    altText: "1",
     src: require("../images/bookappt2.jpeg"),
   },
   {
-    altText: "1",
+    altText: "2",
     src: require("../images/pic1.jpg"),
   },
-  // {
-  //   altText: "2",
-  //   src: require("../images/pic2.jpg"),
-  // },
   {
     altText: "3",
-    src: require("../images/pic3.jpg"),
+    src: require("../images/pic2.jpg"),
   },
   {
     altText: "4",
-    src: require("../images/pic4.jpg"),
+    src: require("../images/pic3.jpg"),
   },
   {
     altText: "5",
-    src: require("../images/pic5.jpg"),
+    src: require("../images/pic4.jpg"),
   },
   {
     altText: "6",
-    src: require("../images/pic6.jpg"),
+    src: require("../images/pic5.jpg"),
   },
   {
     altText: "7",
-    src: require("../images/pic7.jpg"),
+    src: require("../images/pic6.jpg"),
   },
   {
     altText: "8",
-    src: require("../images/pic8.jpg"),
+    src: require("../images/pic7.jpg"),
   },
   {
     altText: "9",
+    src: require("../images/pic8.jpg"),
+  },
+  {
+    altText: "10",
     src: require("../images/pic9.jpg"),
   },
 ];
